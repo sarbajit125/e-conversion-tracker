c339fcb9e0436a9310b3eaddca4c463942766ffd
Sarbajit Biswal <sarbajeet.biswal@gmail.com>
2023-10-01 21:14:36 +0530

    updated prisma for slot booking table

---
+++ src/app/api/create-pauti/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../networking/primsaInstance";
+import { handleErrorInServer } from "../create-ticket/route";
+import { APISuccessResp, SlotTicktFormValidation } from "@/layouts/ComponentsStyle";
+
+export async function POST(request: Request) {
+    try {
+        const requestBody = await request.json();
+        const parsedRequest = await SlotTicktFormValidation.validate(requestBody)
+        const dataFromDb = await prisma.slotBooking_Table.create({
+            data:{
+                application_id: parsedRequest.application_id,
+                district: parsedRequest.district,
+                officeName: parsedRequest.officeName,
+                secondParty: parsedRequest.secondParty,
+                firstParty_Id: {
+                    connectOrCreate:{
+                        where:{
+                            name: parsedRequest.firstParty
+                        },
+                        create:{
+                            name: parsedRequest.firstParty
+                        }
+                    }
+                },
+                slotDate: parsedRequest.slotDate
+            }
+        })
+        const successResp: APISuccessResp = {
+            message: 'Sale deed slot recorded',
+            timeStamp: new Date().toUTCString()
+        }
+        return NextResponse.json(successResp, {status: 201})
+    } catch (error) {
+        const errorObj = handleErrorInServer(error)
+    console.log(errorObj)
+    return NextResponse.json(errorObj, { status: 400 });
+    }
+}
